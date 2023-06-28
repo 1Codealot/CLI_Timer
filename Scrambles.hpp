a6f8297ab00e38a61ce4991a235fde70ad36cb05
1Codealot <58225804+1Codealot@users.noreply.github.com>
2023-06-28 16:50:40 +0100

    Added more scramble types.
    Added: Skewb, Megaminx, Pyraminx. also made it a header file.

--- Scrambles.hpp
+++ Scrambles.hpp
@@ -2,8 +2,6 @@
 #include <cstdlib>
 #include <ctime>
 
-//TODO: Add a \n and input time into the end.
-
 int getRandomNum(int min, int max) {
     return rand() % (max - min) + min;
 }
@@ -28,8 +26,6 @@ std::string Two_By_Two() {
             alg += '2';
         }
         alg += space;
-
-
 
         prevMove[0] = currMove[0];
         while (currMove[0] == prevMove[0]) {
@@ -313,6 +309,168 @@ std::string Seven_By_Seven() {
         prevMove[0] = currMove[0];
         while (currMove[0] == prevMove[0]) {
             currMove[0] = moves[getRandomNum(0, 5+1)];
+        }
+    }
+
+    std::string result;
+
+    // Remove non-printable characters and excessive spaces
+    for (char c : alg) {
+        if (isprint(c)) {
+            result += c;
+        } else if (c == ' ') {
+            if (result.empty() || result.back() != ' ') {
+                result += c;
+            }
+        }
+    }
+
+    return result;
+}
+
+std::string Skewb() {
+    const char space[] = {' '};
+
+    char moves[] = {'U', 'L', 'R', 'B'};
+    char currMove[2] = {moves[getRandomNum(0, 3+1)], '\0'};
+    char prevMove[2] = {'\0'};
+
+    std::string alg;
+
+    for (int i = 0; i < getRandomNum(9, 12); ++i) {
+        alg += currMove;
+        int extra = getRandomNum(1,2+1);
+
+        if (extra == 1)
+        {
+            alg += '\'';
+        }
+        alg += space;
+
+        prevMove[0] = currMove[0];
+        while (currMove[0] == prevMove[0]) {
+            currMove[0] = moves[getRandomNum(0, 2+1)];
+        }
+    }
+
+    std::string result;
+
+    // Remove non-printable characters and excessive spaces
+    for (char c : alg) {
+        if (isprint(c)) {
+            result += c;
+        } else if (c == ' ') {
+            if (result.empty() || result.back() != ' ') {
+                result += c;
+            }
+        }
+    }
+
+    return result;
+}
+
+std::string Megaminx(){
+    const char space[] = {' '};
+    char prevMove[2] = {'\0'};
+    bool Dpp;
+
+    std::string alg;
+
+    for (int n = 0; n < 5; ++n)
+    {    
+        for (int i = 0; i < 7; ++i)
+        {
+            alg += 'R';
+            if (getRandomNum(1,2+1) == 1)
+            {
+                    alg += '+'; alg += '+'; //Bruh
+                    alg += space;
+                }else{
+                    alg += '-'; alg += '-';
+                    alg += space;
+            }
+
+            alg += 'D';
+            if (getRandomNum(1,2+1) == 1)
+            {
+                    alg += '+'; alg += '+';
+                    Dpp = true;
+                    alg += space;
+                }else{
+                    alg += '-'; alg += '-';
+                    Dpp = false;
+                    alg += space;
+            }
+        }
+        if(Dpp){
+            alg += 'U';
+        }else{
+            alg += 'U';
+            alg += '\''; 
+        }
+        alg += '\n';
+    }
+
+     std::string result;
+
+    // Remove non-printable characters and excessive spaces
+    for (char c : alg) {
+        if (isprint(c)) {
+            result += c;
+        } else if (c == ' ') {
+            if (result.empty() || result.back() != ' ') {
+                result += c;
+            }
+        }
+    }
+
+    
+    return alg;  
+}
+
+std::string Pyraminx() {
+    const char space[] = {' '};
+
+    char moves[] = {'U', 'L', 'R', 'B'};
+    char tips[] = {'u', 'l', 'r', 'b'};
+    char currMove[2] = {moves[getRandomNum(0, 3+1)], '\0'};
+    char prevMove[2] = {'\0'};
+
+    std::string alg;
+
+    for (int i = 0; i < getRandomNum(9, 12); ++i) {
+        alg += currMove;
+        int extra = getRandomNum(1,2+1);
+
+        if (extra == 1)
+        {
+            alg += '\'';
+        }
+        alg += space;
+
+        prevMove[0] = currMove[0];
+        while (currMove[0] == prevMove[0]) {
+            currMove[0] = moves[getRandomNum(0, 2+1)];
+        }
+    }
+
+    char TcurrMove[2] = {tips[getRandomNum(0, 3+1)], '\0'};
+    char TprevMove[2] = {'\0'};
+
+    // Tips
+    for (int i = 0; i < getRandomNum(1, 4); ++i) {
+        alg += TcurrMove;
+        int extra = getRandomNum(1,2+1);
+
+        if (extra == 1)
+        {
+            alg += '\'';
+        }
+        alg += space;
+
+        TprevMove[0] = TcurrMove[0];
+        while (TcurrMove[0] == TprevMove[0]) {
+            TcurrMove[0] = tips[getRandomNum(0, 3+1)];
         }
     }
 
