827b8fd04d5b6121ec1e143a3f8bea6cc2f56633
1Codealot <58225804+1Codealot@users.noreply.github.com>
2023-08-16 13:09:26 +0100

    Created fileIO and implemented it.
    I know there is not O but idc rn

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "Scrambles.hpp"
+#include "fileIO.hpp"
 
 using namespace std;
 
@@ -7,9 +8,12 @@ float inputtedTime;
 
 int main(int argc, char const *argv[])
 {
+
+	const bool shouldContinue = (argv[2][0] == 'c' || (argv[2][0] == '-' && argv[2][1] == 's'));
 	if (argc == 1)
 	{
 		cout<<"You fogor commands lul.";
+		return 1;
 	}
 
 	srand(static_cast<unsigned>(time(NULL)));
@@ -18,81 +22,101 @@ int main(int argc, char const *argv[])
 		{
 		do
 			{
-				cout<<Three_By_Three();
+				string currentScramble = Three_By_Three();
+				cout<<currentScramble<<"\nEnter your time: ";
 				cin>>inputtedTime;
-			} while (*argv[2] == 'c');	}
+				save(argv[2], currentScramble, inputtedTime);
+			} while (shouldContinue);	}
 
 	else if(*argv[1] == '2')
 		{
 		do
 			{
-				cout<<Two_By_Two();
+				string currentScramble = Two_By_Two();
+				cout<<currentScramble<<"\nEnter your time: ";
 				cin>>inputtedTime;
-			} while (*argv[2] == 'c');	}
+				save(argv[2], currentScramble, inputtedTime);
+			} while (shouldContinue);	}
 
 	else if(*argv[1] == '4')
 		{
 		do
 			{
-				cout<<Four_By_Four();
+				string currentScramble = Four_By_Four();
+				cout<<currentScramble<<"\nEnter your time: ";
 				cin>>inputtedTime;
-			} while (*argv[2] == 'c');	}
+				save(argv[2], currentScramble, inputtedTime);
+			} while (shouldContinue);	}
 
 	else if(*argv[1] == '5')
 		{
 		do
 			{
-				cout<<Five_By_Five();
+				string currentScramble = Five_By_Five();
+				cout<<currentScramble<<"\nEnter your time: ";
 				cin>>inputtedTime;
-			} while (*argv[2] == 'c');	}
+				save(argv[2], currentScramble, inputtedTime);
+			} while (shouldContinue);	}
 
 	else if(*argv[1] == '6')
 		{
 		do
 			{
-				cout<<Six_By_Six();
+				string currentScramble = Six_By_Six();
+				cout<<currentScramble<<"\nEnter your time: ";
 				cin>>inputtedTime;
-			} while (*argv[2] == 'c');	}
+				save(argv[2], currentScramble, inputtedTime);
+			} while (shouldContinue);	}
 
 	else if(*argv[1] == '7')
 		{
 		do
 			{
-				cout<<Seven_By_Seven();
+				string currentScramble = Seven_By_Seven();
+				cout<<currentScramble<<"\nEnter your time: ";
 				cin>>inputtedTime;
-			} while (*argv[2] == 'c');	}
+				save(argv[2], currentScramble, inputtedTime);
+			} while (shouldContinue);	}
 
 	else if(*argv[1] == 'S')
 		{
 		do
 			{
-				cout<<Skewb();
+				string currentScramble = Skewb();
+				cout<<currentScramble<<"\nEnter your time: ";
 				cin>>inputtedTime;
-			} while (*argv[2] == 'c');	}
+				save(argv[2], currentScramble, inputtedTime);
+			} while (shouldContinue);	}
 
 	else if(*argv[1] == 'M')
 		{
 		do
 			{
-				cout<<Megaminx();
+				string currentScramble = Megaminx();
+				cout<<currentScramble<<"\nEnter your time: ";
 				cin>>inputtedTime;
-			} while (*argv[2] == 'c');	}
+				save(argv[2], currentScramble, inputtedTime);
+			} while (shouldContinue);	}
 
 	else if(*argv[1] == 'P')
 		{
 		do
 			{
-				cout<<Pyraminx();
+				string currentScramble = Pyraminx();
+				cout<<currentScramble<<"\nEnter your time: ";
 				cin>>inputtedTime;
-			} while (*argv[2] == 'c');	}
+				save(argv[2], currentScramble, inputtedTime);
+			} while (shouldContinue);	}
 
 	else if(*argv[1] == 'K')
 		{
 		do
 			{
-				cout<<Clock();
+				string currentScramble = Clock();
+				cout<<currentScramble<<"\nEnter your time: ";
 				cin>>inputtedTime;
-			} while (*argv[2] == 'c');	}
+				save(argv[2], currentScramble, inputtedTime);
+			} while (shouldContinue);	}
 
 	return 0;
 }
