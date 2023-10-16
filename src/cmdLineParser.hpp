9a530a33709f6b6d17f6b067f422b494cd467e7b
1Codealot <noahpoulton2@gmail.com>
2023-10-16 18:40:15 +0100

    Fixed no continuing with -s and fixed filename bug

--- src/cmdLineParser.hpp
+++ src/cmdLineParser.hpp
@@ -30,7 +30,6 @@ char getCubeType(cmdLineArgs){
     }
 }
 
-
 bool shouldSave(cmdLineArgs){
     for (int i = 1; i < argc; ++i) {
         if (argv[i][0] == '-' && argv[i][1] == 's'){
@@ -50,7 +49,7 @@ std::string getFileName(cmdLineArgs){
                     std::cerr<<"You tried to save to a file but you didn't give it a name\n\n";
                     exit(EXIT_FAILURE);
                 } else {
-                    return fileName.substr(2, fileName.length());
+                    return fileName;
                 }
             }
         }
@@ -59,6 +58,11 @@ std::string getFileName(cmdLineArgs){
 }
 
 bool shouldContinue(cmdLineArgs){
+    if (shouldSave(argc, argv))
+    {
+        return true;
+    }
+    
     // Oops, turned on python mode. lmao
     for (int i = 1; i < argc; ++i)
         if (*argv[i] == 'c')
@@ -84,7 +88,14 @@ void setup(struct should &Options, cmdLineArgs){
     // Pre-checks
     // These are for like `help` or `--version`
 
-    std::string helpMSG = "How to use CLI_Timer.\nCLI_Timer (cube type) [c] | [-s{session name}] | [--no_prompt] \n\nArgument (cube type) means an NxN of (2)x2 (3)x3 to (7)x7 or (S)kewb, (P)yraminx, (M)egaminx, or (C)lock. It is required (why else would you use it?)\n\n[c] means [c]ontinuous, meaning it won't stop after generating one scramble\n\nArgument [-s] is for saving to a file which name will come directly after [-s] (e.g. CLI_Timer 3 -s3x3_One_Handed). \nIt will save to a .CLI_T_S (CLI_Timer_Session) file. Check README.md to see where it goes on your OS.\n\nArgument [--no_prompt] will just generate a scramble, not ask for a time (for this reason you can't have [-s] with this), wait for an enter, then generate another one.\n\nCLI_Timer (--version)\nOutputs the current version of CLI_Timer\n\nCLI_Timer (help)\nOutputs this.\n\nAny issues, put them on the GitHub repo.";
+    std::string helpMSG = "How to use CLI_Timer.\nCLI_Timer (cube type) [c] | [-s{session name}] | [--no_prompt]\
+    \n\nArgument (cube type) means an NxN of (2)x2 (3)x3 to (7)x7 or (S)kewb, (P)yraminx, (M)egaminx, or (C)lock.\
+    It is required (why else would you use it?)\n\n[c] means [c]ontinuous, meaning it won't stop after generating one scramble\
+    \n\nArgument [-s] is for saving to a file which name will come directly after [-s] (e.g. CLI_Timer 3 -s3x3_One_Handed)\
+    \nIt will save to a .CLI_T_S (CLI_Timer_Session) file. Check README.md to see where it goes on your OS.\
+    \n\nArgument [--no_prompt] will just generate a scramble, not ask for a time (for this reason you can't have [-s] with this), wait for an enter, then generate another one.\
+    \n\nCLI_Timer (--version)\nOutputs the current version of CLI_Timer\n\nCLI_Timer (help)\nOutputs this.\
+    \n\nAny issues, put them on the GitHub repo.";
 
     if (argc == 1){
         std::cout<<helpMSG<<std::endl;
