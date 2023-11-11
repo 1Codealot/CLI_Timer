e7c72005319beb03715cc75d444b2452868fa028
1Codealot <58225804+1Codealot@users.noreply.github.com>
2023-11-11 08:11:37 +0000

    Update change log

--- src/cmdLineParser.hpp
+++ src/cmdLineParser.hpp
@@ -114,7 +114,7 @@ void setup(struct should &Options, cmdLineArgs){
     // Pre-checks
     // These are for like `help` or `--version`
 
-    std::string helpMSG = "How to use CLI_Timer.\nCLI_Timer (cube type) [--count{number}] | [c] | [-s{session name}] | [--no_prompt] | [--no_avg] \
+    std::string helpMSG { "How to use CLI_Timer.\nCLI_Timer (cube type) [--count{number}] | [c] | [-s{session name}] | [--no_prompt] | [--no_avg] \
     \n\nArgument (cube type) means an NxN of (2)x2 (3)x3 to (7)x7 or (S)kewb, (P)yraminx, (M)egaminx, (C)lock or s(Q)uare-1.\
     It is required (why else would you use it?)\n\n[c] means [c]ontinuous, meaning it won't stop after generating one scramble.\
     \n--count{number} can be used to specify how many scrambles you want. Continuous is impiled. It will then quit (with code 0).\
@@ -127,7 +127,7 @@ void setup(struct should &Options, cmdLineArgs){
     \n\nArgument [--no_avg] means that it will not show the averages\
     \n\nCLI_Timer (--version)\nOutputs the current version of CLI_Timer\n\nCLI_Timer (help)\nOutputs this.\
     \n\nAny issues, put them on the GitHub repo: https://github.com/1Codealot/CLI_Timer/issues\
-    \n\nLICENCES: Main: MIT licences.\nSquare-1 code: GNU General Public License v3.0 (repo: <https://github.com/thewca/tnoodle-lib>)";
+    \n\nLICENCES: Main: MIT licences.\nSquare-1 code: GNU General Public License v3.0 (repo: <https://github.com/thewca/tnoodle-lib>)" }; // Used braces so that I can fold it.
 
     if (argc == 1){
         std::cout<<helpMSG<<std::endl;
@@ -136,7 +136,7 @@ void setup(struct should &Options, cmdLineArgs){
         std::cout<<helpMSG<<std::endl;
         exit(EXIT_SUCCESS);
     } else if (std::string(argv[1]) == "--version"){
-        std::cout<<"CLI_Timer version: 1.12.0.3\n\n";
+        std::cout<<"CLI_Timer version: 1.12\n\n";
         std::cout<<"Changes: Added file averages AND temporary session averages"<<std::endl;
         std::cout<<"Added [--no_avg] to not show averages"<<std::endl;
         exit(EXIT_SUCCESS);
