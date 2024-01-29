86563190aee765fc739849f67b1a5ffd9a2940ee
1Codealot <58225804+1Codealot@users.noreply.github.com>
2024-01-29 18:16:35 +0000

    Update changelog

--- src/outputting.hpp
+++ src/outputting.hpp
@@ -318,5 +318,5 @@ void outputVersion()
 	std::cout << "Use a vector to hold moves" << std::endl;
 	std::cout << "Added a cache to hold scrambles" << std::endl;
 	std::cout << "Fixed +2 being ignored bug" << std::endl;
-	std::cout << "Fixed bug with megaminx outputting." << std::endl;
+	std::cout << "Fixed bug with megaminx outputting. And square-1" << std::endl;
 }
