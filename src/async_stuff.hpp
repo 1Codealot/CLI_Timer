9aff530535dad647c395ff12f153e2a2cac346b5
1Codealot <58225804+1Codealot@users.noreply.github.com>
2024-01-30 10:10:18 +0000

    add async_stuff.hpp
    im so dum

---
+++ src/async_stuff.hpp
@@ -0,0 +1,30 @@
+#pragma once
+#include <queue>
+#include <string>
+#include <chrono>
+
+#include "scrambles.hpp"
+#include "command_line_parser.hpp"
+
+void update_cache(std::queue<std::string> *cache, const should *Args)
+{
+    while (Args->shouldContinue && Args->scrambleCount != 0)
+    {
+        if (cache->size() < Args->cache_size)
+        {
+            cache->push(generate_scramble(Args->cubeType, Args->blindfolded, Args->fmc));
+            std::this_thread::sleep_for(std::chrono::milliseconds(200*(Args->needEnter)));
+        }
+    }
+}
+
+static int lastTerminalWidth = get_terminal_width();
+
+void update_terminal(std::string *scramble, const should *Args){
+    while (Args->shouldContinue && Args->scrambleCount != 0){
+	if (lastTerminalWidth != get_terminal_width()){
+	    lastTerminalWidth = get_terminal_width();
+
+
+    }
+}
